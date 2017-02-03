6dc70c37fb326b55901349b4de5c6bf8b35d59b2
Nakurai <cmamessier@gmail.com>
2017-02-03 18:50:07 -0500

    added database knowledge

---
+++ res/decision-tree.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+      income:60000, // beyond this amount, the option is not possible anymore
+      maritalStatus:'married',
+      issues:['child-support', 'spousal-support', 'division-assets'],
+      option:'trial' // code of the option
+    }
+];
---
+++ res/options.js
@@ -0,0 +1,31 @@
+var options = {};
+options['trial'] =
+  {
+    code:'trial',
+    name:'Trial',
+    cost:232000,
+    duration:'around three years'
+  };
+options['mediation'] =
+  {
+    code:'mediation',
+    name:'Mediation',
+    cost:15000,
+    duration:'In general, mediation takes less time than going to trial.'
+  };
+options['agreement'] =
+  {
+    code:'agreement',
+    name:'Separation agreement',
+    cost:4000,
+    duration:'In general, mediation takes less time than going to trial.'
+  };
+options['selfrepresentation'] =
+  {
+    code:'selfrepresentation',
+    name:'Self representation',
+    cost:0,
+    duration:'In general, self representation takes A LOT of personal and resources.'
+  };
+
+module.exports = options;
