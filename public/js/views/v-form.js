ad85800ae7599904948a34bf7d13541fd1302e1e
Amin Adam <Amin Adam>
2017-02-03 15:17:25 -0500

    the questions have been entered

--- public/js/views/v-form.js
+++ public/js/views/v-form.js
@@ -16,6 +16,34 @@ vForm.prototype = {
     */
     html: function(){
       return '<div>\
+      <form class="w3-container">\
+  <fieldset>\
+    <legend>Annual Income</legend>\
+    <label>Your Annual Income</label>\
+    <input class="w3-input" type="text">\
+
+    <label>Spouse Annual Income</label>\
+    <input class="w3-input" type="text">\
+  </fieldset>\
+
+  <fieldset>\
+    <legend>Family</legend>\
+    <label>Number of Children</label>\
+    <input class="w3-input" type="text">\
+
+    <label>Age of child 1</label>\
+    <input class="w3-input" type="text">\
+  </fieldset>\
+
+  <fieldset>\
+    <legend>Family</legend>\
+    <label>Number of Children</label>\
+    <input class="w3-input" type="text">\
+
+    <label>Age of child 1</label>\
+    <input class="w3-input" type="text">\
+  </fieldset>\
+</form>\
       </div>';
     }
 
